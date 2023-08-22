09f90294bf8cc4bdf15106b129e56bc0f55e09f8
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-22 21:55:13 +0200

    feat(Navigation): added Contact to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Navigation from "@/components/Navigation";
+import Contact from "@/components/contact/Contact";
 
 export const metadata = {
     title: 'Space Mobile House - Strona główna',
@@ -10,13 +11,16 @@ export default function RootLayout({children}) {
     return (
         <html lang="pl">
         <body>
-        <div className={`bg-primary w-full min-h-screen px-4 min-w-[300px] overflow-hidden`}>
+        <div className={`bg-primary w-full min-h-screen min-w-[300px] overflow-hidden`}>
             <div className={` mx-auto max-w-[1200px] w-full flex-col justify-center items-around h-[100%] px-4 relative`}>
                 <Navigation/>
             </div>
             <main className="relative flex justify-center items-center flex-col">
                 {children}
             </main>
+            <div className={`w-full flex-col justify-center items-around h-[100%] relative`}>
+                <Contact/>
+            </div>
         </div>
         </body>
         </html>
