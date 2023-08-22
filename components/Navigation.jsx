d8ffe0d4785a023d352813815bcd986995356a6d
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-22 21:56:23 +0200

    feat(Navigation): changed position of navbar

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -10,13 +10,13 @@ const Navigation = () => {
         <nav className="w-full flex p-[40px] justify-between items-center navbar text-primary absolute">
             <a href="/" className="cursor-pointer flex flex-col items-center z-[6]">
                 <img src='/assets/icons/home-outline.svg' alt="home icon" className="w-[28px] h-[28px] sm:flex hidden"/>
-                <h1 className="font-bold text-3xl tracking-wider">SMH</h1>
+                <h1 className="font-bold text-white text-3xl tracking-wider">SMH</h1>
             </a>
             <div className="flex flex-col">
                 <ul className="list-none sm:flex hidden justify-end items-center flex-1 pr-5 z-[6]">
                     {navLinks.map((nav, index) => (
                         <li key={nav.id}
-                            className={`p-1 hover:underline hover:decoration-solid hover:decoration-third hover:underline-offset-8 font-bricolage cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} tracking-wider`}>
+                            className={`p-1 hover:underline hover:decoration-solid hover:decoration-primary hover:underline-offset-8 font-bricolage cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} tracking-wider`}>
                             <Link href={`/${nav.id}`}>{nav.title}</Link>
                         </li>))}
                 </ul>
@@ -25,7 +25,7 @@ const Navigation = () => {
                          className="w-[28px] h-[28px] cursor-pointer"
                          onClick={() => setToggle((prev) => !prev)}/>
                     <div
-                        className={`${toggle ? 'flex' : 'hidden'} p-3 absolute top-20 right-10 mx-4 my-2 min-w-[140px] rounded-xl bg-fourth`}>
+                        className={`${toggle ? 'flex' : 'hidden'} p-3 absolute top-20 right-5 mx-4 my-2 min-w-[140px] rounded-xl bg-green`}>
                         <ul className="list-none flex flex-col justify-end items-center flex-1 z-[6]">
                             {navLinks.map((nav, index) => (
                                 <li key={nav.id}
