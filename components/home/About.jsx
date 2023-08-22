856791460e3d49450bcfbeb545c33923b5f86459
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-22 21:07:45 +0200

    feat(About): added video for about section

---
+++ components/home/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+      <section className="w-full min-h-screen">
+          <div className="relative bg-gradient-to-b from-green to-transparent custom-shape px-3">
+              <video src="/assets/video/home.mp4" autoPlay loop muted
+                     className="custom-shape-video"></video>
+          </div>
+      </section>
+  )
+}
+export default About
