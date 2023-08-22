0381e39d2f539651cee43103754c73ea87d67f1b
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-22 21:59:02 +0200

    feat(HeaderHome): changed styles in Header

--- components/home/HeaderHome.jsx
+++ components/home/HeaderHome.jsx
@@ -5,10 +5,10 @@ const HeaderHome = () => {
     return (
         <motion.div
             variants={move2} initial="hidden" whileInView="show"
-            className="flex flex-col justify-center items-center absolute h-[80%] z-[5]">
+            className="flex flex-col justify-end xs:justify-center items-center absolute h-[80%] z-[5]">
             <h1 className="font-bricolage text-6xl uppercase p-3"> Space Mobile House</h1>
             <h2 className="font-bricolage text-3xl p-3">Twoja wymarzona przestrzeń</h2>
-            <button className="bg-fourth rounded-[20px] p-3" > Sprawdź nasze projekty</button>
+            <button className="bg-green text-white rounded-[20px] p-3" > Sprawdź nasze domy szkieletowe</button>
         </motion.div>
     )
 }
