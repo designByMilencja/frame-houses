e069e150c6a6f848bbbf1efb62d501fc0cb8eb7a
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-25 16:36:14 +0200

    feat(Video): added Video component

---
+++ components/home/Video.jsx
@@ -0,0 +1,9 @@
+const Video = () => {
+  return (
+      <div className="relative bg-gradient-to-b from-green to-transparent custom-shape px-3">
+          <video src="/assets/video/home.mp4" autoPlay loop muted
+                 className="rounded-[20px] custom-shape-video"></video>
+      </div>
+  )
+}
+export default Video
