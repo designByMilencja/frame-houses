934c21953ab436f000308ed7633b8d17b78a110a
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-27 09:34:48 +0200

    feat(Backgrounds): added shadow to box

--- components/home/Backgrounds.jsx
+++ components/home/Backgrounds.jsx
@@ -6,7 +6,7 @@ const Backgrounds = ({backgroundIndex, handleBackgroundChange}) => {
         {backgroundsSmall.map((bg, index) => (
             <div key={index} className="mx-2 bg-fourth flex">
             <div
-                className={`w-[150px] h-[150px] bg-cover bg-center m-2 cursor-pointer 
+                className={`w-[150px] h-[150px] bg-cover bg-center m-2 cursor-pointer box-with-shadow
                                     ${backgroundIndex === index ? 'border-4 border-primary' : ''}`}
                 style={{ backgroundImage: `url("${bg}")` }}
                 onClick={() => handleBackgroundChange(index)}
