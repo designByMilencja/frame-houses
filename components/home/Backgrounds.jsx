5e1cc48fb346f6d967f8bbf070f3a327a1c5ddc6
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-27 10:49:34 +0200

    fix(): changed background

--- components/home/Backgrounds.jsx
+++ components/home/Backgrounds.jsx
@@ -4,7 +4,7 @@ const Backgrounds = ({backgroundIndex, handleBackgroundChange}) => {
   return (<div className="absolute h-[80%] flex items-end hidden sm:block top-[70%] z-20">
       <div className="flex">
         {backgroundsSmall.map((bg, index) => (
-            <div key={index} className="mx-2 bg-fourth flex">
+            <div key={index} className="mx-2 flex">
             <div
                 className={`w-[150px] h-[150px] bg-cover bg-center m-2 cursor-pointer box-with-shadow
                                     ${backgroundIndex === index ? 'border-4 border-primary' : ''}`}
