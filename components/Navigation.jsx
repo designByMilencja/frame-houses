728d51a8f5454b4b28d692a95caea08a2af089f1
Milena Pieńkosz <designbymilencja@gmail.com>
2023-08-27 11:33:41 +0200

    fix(globals): increased padding

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -7,13 +7,13 @@ const Navigation = () => {
     const [toggle, setToggle] = useState(false);
 
     return (
-        <nav className="w-full my-5 flex py-3 px-2 rounded-[20px] justify-between items-center text-primary absolute">
-            <a href="/" className="cursor-pointer flex flex-col items-center z-[6] relative">
+        <nav className="w-full my-5 flex py-8 px-2 rounded-[20px] justify-between items-center text-primary absolute">
+            <a href="/" className="cursor-pointer flex flex-col justify-center items-center z-[6] relative">
                 <img src='/assets/icons/home-outline.svg' alt="home icon" className="absolute w-[68px] h-[68px] sm:flex hidden z-[7] top-[-20px]"/>
                 <h1 className="font-bold text-white text-3xl tracking-wider z-[8]">SMH</h1>
             </a>
             <div className="flex flex-col">
-                <ul className="list-none sm:flex hidden justify-end items-center flex-1 pr-5 z-[6]">
+                <ul className="list-none sm:flex hidden justify-end items-center flex-1 pr-[30px] z-[6]">
                     {navLinks.map((nav, index) => (
                         <li key={nav.id}
                             className={`p-1 hover:underline hover:decoration-solid hover:decoration-primary hover:underline-offset-8 font-bricolage cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} tracking-wider`}>
