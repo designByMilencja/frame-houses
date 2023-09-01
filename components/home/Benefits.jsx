d46dfe19a9c167f32dd9bc8ed8978325eff50c8b
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-01 12:17:43 +0200

    feat(Benefits): checked response

--- components/home/Benefits.jsx
+++ components/home/Benefits.jsx
@@ -7,8 +7,8 @@ const Benefits = () => {
                 <div className="flex flex-1 flex-row flex-wrap justify-center">
                     {benefits.map((benefit, index) => (
                         <div
-                            className="circle bg-gradient-to-b from-dark to-transparent box-with-shadow m-6 ">
-                            <div key={index} className="w-[300px] text-text m-3">
+                            className="circle bg-gradient-to-b from-dark to-transparent box-with-shadow my-5 sm:mx-5 ">
+                            <div key={index} className="w-[300px] xs:w-[400px] ss:w-[450px] sm:w-[300px] md:w-[400px]   text-text m-3">
                                 <h2 className="text-center text-2xl px-3 py-4 font-bold">{benefit.title}</h2>
                                 <p className="text-center text-sm px-3 py-4">
                                     {benefit.desc}</p>
