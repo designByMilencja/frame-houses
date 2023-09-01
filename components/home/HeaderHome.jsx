d18aa4bf3b844a73d4f19d14d46dc2b24751fe43
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-01 18:29:07 +0200

    feat(): changed colors of heading

--- components/home/HeaderHome.jsx
+++ components/home/HeaderHome.jsx
@@ -1,3 +1,4 @@
+'use client'
 import {motion} from "framer-motion";
 import {move2} from "@/utils/motion";
 import Button from "@/components/Button";
@@ -6,9 +7,9 @@ const HeaderHome = () => {
     return (
         <motion.div
             variants={move2} initial="hidden" whileInView="show"
-            className="flex flex-col justify-end xs:justify-center items-center absolute h-[80%] z-[5]">
-            <h1 className="font-bricolage text-6xl uppercase p-3"> Space Mobile House</h1>
-            <h2 className="font-bricolage text-3xl p-3">Twoja wymarzona przestrzeń</h2>
+            className="flex flex-col justify-center items-center absolute h-[80%] z-[5]">
+            <h1 className="font-bricolage text-4xl xs:text-6xl uppercase p-3 text-primary text-center"> Space Mobile House</h1>
+            <h2 className="font-bricolage text-2xl xs:text-3xl p-8 text-primary text-center">Twoja wymarzona przestrzeń</h2>
             <Button text="Sprawdź nasze domy szkieletowe"/>
         </motion.div>
     )
