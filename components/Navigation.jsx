e4464b83b2525193632ea0095afbe6bb058127e7
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-02 21:06:14 +0200

    fix(): changed colors

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -7,9 +7,9 @@ const Navigation = () => {
     const [toggle, setToggle] = useState(false);
 
     return (
-        <nav className="w-full flex p-8 rounded-[20px] justify-between items-center text-white absolute">
+        <nav className="w-full flex p-8 rounded-[20px] justify-between items-center text-primary absolute">
             <a href="/" className="cursor-pointer flex flex-col justify-center items-center z-[6]">
-                <img src='/assets/icons/home-outline.svg' alt="home icon" className="w-[48px] h-[48px] sm:flex hidden z-[7]"/>
+                <img src='/assets/icons/home-outline.svg' alt="home icon" className="w-[48px] h-[48px] sm:flex hidden z-[7] mt-[20px]"/>
                 <h1 className="font-bold text-primary text-3xl tracking-wider z-[8]">SMH</h1>
             </a>
             <div className="flex flex-col">
@@ -25,11 +25,11 @@ const Navigation = () => {
                          className="w-[38px] h-[38px] cursor-pointer mx-3"
                          onClick={() => setToggle((prev) => !prev)}/>
                     <div
-                        className={`${toggle ? 'flex' : 'hidden'} p-3 absolute top-20 right-5 mx-4 my-2 min-w-[140px] rounded-xl bg-green`}>
+                        className={`${toggle ? 'flex' : 'hidden'} p-3 absolute top-20 right-5 mx-4 my-2 min-w-[140px] rounded-xl bg-dark`}>
                         <ul className="list-none flex flex-col justify-end items-center flex-1 z-[6]">
                             {navLinks.map((nav, index) => (
                                 <li key={nav.id}
-                                    className={`font-bricolage text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} cursor-pointer text-white z-[6]`}>
+                                    className={`font-bricolage text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} cursor-pointer text-primary z-[6]`}>
                                     <Link href={`/${nav.id}`} className="z-[6]">{nav.title}</Link></li>))}
                         </ul>
                     </div>
