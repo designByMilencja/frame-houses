3ee7bfcfacd79b39b4ad41a8e8704c125be5d5e4
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-06 23:04:15 +0200

    feat(Header Home): added shadow for text

--- components/home/HeaderHome.jsx
+++ components/home/HeaderHome.jsx
@@ -8,7 +8,7 @@ const HeaderHome = ({h1, h2, text}) => {
         <motion.div
             variants={move2} initial="hidden" whileInView="show"
             className="flex flex-col justify-center items-center absolute h-[80%] z-[5]">
-            <h1 className="font-bricolage text-4xl xs:text-6xl uppercase p-3 text-primary text-center"> {h1}</h1>
+            <h1 className="font-bricolage text-with-shadow text-4xl xs:text-6xl uppercase p-3 text-primary text-center"> {h1}</h1>
             <h2 className="font-bricolage text-2xl xs:text-3xl p-8 text-primary text-center">{h2}</h2>
             <Button text={text}/>
         </motion.div>
