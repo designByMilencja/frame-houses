1571b807738eb7b81fab5cb4c27fd68daf847a6e
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-08 14:49:08 +0200

    feat(Navigation): added handling for showing and hiding mobile navigation

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,41 +1,59 @@
 'use client'
 import {navLinks} from "@/constants";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Link from "next/link";
-import { usePathname} from 'next/navigation'
+import {usePathname} from 'next/navigation'
 
 
 const Navigation = () => {
     const [toggle, setToggle] = useState(false);
-    const pathname = usePathname()
-    console.log(pathname)
+    const pathname = usePathname();
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (toggle && !event.target.closest('.close')) {
+                setToggle(false);
+            }
+        };
+        document.body.addEventListener('click', handleClickOutside);
+        return () => {
+            document.body.removeEventListener('click', handleClickOutside);
+        };
+    }, [toggle]);
 
     return (
-        <nav className="w-[96%] flex justify-between items-center text-primary absolute z-[6] bg-dark rounded-[20px] mt-[120px] p-3">
-            <a href="/" className="cursor-pointer flex justify-center items-center z-[6] px-6">
+        <nav
+            className="w-[95%] flex justify-between items-center fixed text-green bg-primary shadow rounded-[15px] z-[6] mt-[120px] p-3">
+            <a href="/" className="flex justify-center items-center bg-green rounded-[15px] cursor-pointer px-6">
                 <img src='/assets/icons/home-outline.svg' alt="home icon" className="w-[48px] h-[48px] sm:flex hidden"/>
-                <h1 className="font-bold text-primary text-3xl tracking-wider text-with-shadow">SMH</h1>
+                <h1 className="text-3xl font-bold text-primary tracking-wider p-2">SMH</h1>
             </a>
             <div className="flex flex-col">
-                <ul className="list-none sm:flex hidden justify-end items-center flex-1 pr-[30px] z-[6]">
-                    {navLinks.map((nav, index) => (
-                        <li key={nav.id}
-                            className={`p-1 cursor-pointer text-[16px] 
-                            ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} tracking-wider ${pathname === `/${nav.id}` ? 'active' : ''}`}>
+                <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+                    {navLinks.map((nav) => {
+                        const isActive = pathname === `/${nav.id}`;
+                        return (<li key={nav.id}
+                                    className={`${isActive ? 'active' : ''}
+                            ${isActive ? '' : 'hover:underline hover:decoration-green hover:decoration-2 hover:underline-offset-8'}
+                            cursor-pointer text-[16px] tracking-wide p-1 mr-5 `}>
                             <Link href={`/${nav.id}`}>{nav.title}</Link>
-                        </li>))}
+                        </li>)
+                    })}
                 </ul>
-                <div className="sm:hidden flex flex-1 justify-end items-center pr-5 z-[6]">
+                <div className="sm:hidden flex flex-1 justify-end items-center">
                     <img src='/assets/icons/home-outline.svg' alt="home icon"
-                         className="w-[38px] h-[38px] cursor-pointer mx-3"
+                         className="close w-[48px] h-[48px] bg-green rounded-[15px] cursor-pointer mx-3 p-2"
                          onClick={() => setToggle((prev) => !prev)}/>
                     <div
-                        className={`${toggle ? 'flex' : 'hidden'} p-3 absolute top-20 right-5 mx-4 my-2 min-w-[140px] rounded-xl bg-dark`}>
-                        <ul className="list-none flex flex-col justify-end items-center flex-1 z-[6]">
-                            {navLinks.map((nav, index) => (
-                                <li key={nav.id}
-                                    className={`font-bricolage text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} cursor-pointer text-primary z-[6]`}>
-                                    <Link href={`/${nav.id}`} className="z-[6]">{nav.title}</Link></li>))}
+                        className={`${toggle ? 'flex' : 'hidden'} absolute top-20 right-2 min-w-[140px] rounded-[15px] bg-primary my-2 px-5 py-4`}>
+                        <ul className="list-none flex flex-col justify-end items-center flex-1">
+                            {navLinks.map((nav) => {
+                                const isActive = pathname === `/${nav.id}`;
+                                return (<li key={nav.id}
+                                            className={`${isActive ? 'active' : ''}
+                                     ${isActive ? '' : 'hover:underline hover:decoration-green hover:decoration-2 hover:underline-offset-8'}
+                                    w-[100%] text-center font-bricolage text-[16px] cursor-pointer text-green mb-2`}>
+                                    <Link href={`/${nav.id}`}>{nav.title}</Link></li>)
+                            })}
                         </ul>
                     </div>
                 </div>
