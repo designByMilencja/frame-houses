c1bd88785d9d01a7ee32e0e1d87552fe7f7ca9ef
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-08 16:34:34 +0200

    feat(HeroHouses): changed styles

--- components/houses/HeroHouses.jsx
+++ components/houses/HeroHouses.jsx
@@ -1,10 +1,10 @@
 
 const HeroHouses = () => {
     return (<>
-            <div className="absolute inset-0 rounded-[20px] overflow-hidden z-[1]">
-                <video src="/assets/video/house.mp4" className="absolute p-3 w-full h-full object-cover transition-all duration-2000 rounded-[20px]" autoPlay muted></video>
+            <div className="shadow absolute h-[97%] w-[97%] rounded-[15px] overflow-hidden z-[1]">
+                <video src="/assets/video/house.mp4" className="absolute w-full h-full object-cover transition-all duration-2000 rounded-[15px]" autoPlay muted></video>
             </div>
-            <div className="absolute w-[99%] h-[99%] bg-black opacity-30 rounded-[20px] z-[2]"></div>
+            <div className="absolute h-[98%] w-[98%] bg-text opacity-20 rounded-[15px] z-[2]"></div>
         </>
     )
 }
