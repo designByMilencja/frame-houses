f0077240058d24fc6404467840592955b8c5f170
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-08 16:23:43 +0200

    feat(Hero): changed color of shadow

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({backgroundIndex}) => {
                 className="shadow absolute h-[97%] w-[97%] bg-cover bg-center rounded-[15px] transition-all duration-2000"
                 style={{backgroundImage: `url("${backgrounds[backgroundIndex]}")`}}>
             </div>
-            <div className="absolute h-[98%] w-[98%] bg-black opacity-30 rounded-[15px] z-[1]"></div>
+            <div className="absolute h-[98%] w-[98%] bg-text opacity-30 rounded-[15px] z-[1]"></div>
         </>
     )
 }
