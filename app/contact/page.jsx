bba14d906ed49b88e2202b3000d65a1286248ba6
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-11 22:26:23 +0200

    fix(page): changed content of contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,18 +1,19 @@
 import HeaderHome from "@/components/home/HeaderHome";
-import Form from "@/components/contact/Form";
+import ContactForm from "@/components/contact/ContactForm";
+import Invite from "@/components/contact/Invite";
 
 export default function Houses() {
     return (
         <>
             <section id="contact"
                      className="w-full h-screen max-w-[1700px] flex justify-center items-center text-text bg-primary relative">
-                <HeaderHome h1="Space Mobile House" h2="Rozwiej swoje wątpliwości i napisz do nas" text="Przejdź do kontaktu"/>
+                <HeaderHome h1="Space Mobile House" h2="Napisz do nas, by rozwiać swoje wątpliwości" text="Przejdź do kontaktu"/>
                 <div className="bg-cover bg-center w-[98%] h-[98%] rounded-[15px]" style={{backgroundImage: "url('/assets/images/pawel-czerwinski-Ub0pkOC02Sg-unsplash.jpg')"}}></div>
             </section>
             <section id="houses-types"
-                     className="w-full max-w-[1700px] flex justify-center items-center text-text bg-primary relative">
-                <h3>Prześlij nam wiadomość a skontaktujemy się z Tobą najszybciej jak to możliwe</h3>
-                <Form/>
+                     className="w-full max-w-[1200px] flex flex-col justify-center items-center text-text relative">
+                <Invite/>
+                <ContactForm/>
             </section>
         </>
     )
