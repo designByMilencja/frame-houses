72fffff39118c16af2f30d0bf27d062cff051e72
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-13 23:10:20 +0200

    feat(page): added Production page

---
+++ app/production/page.jsx
@@ -0,0 +1,21 @@
+import HeaderHome from "@/components/home/HeaderHome";
+import ProductionBox from "@/components/production/ProductionBox";
+
+export default function Production() {
+    return (
+        <>
+            <section id="production"
+                     className="w-full h-screen max-w-[1700px] flex justify-center items-center text-text bg-primary relative">
+                <HeaderHome h1="Space Mobile House" h2="Poznaj etapy budowy naszych domów" text="Przejdź do kontaktu"/>
+                <div className="shadow absolute bg-cover bg-center h-[97%] w-[97%] rounded-[15px] overflow-hidden z-[1]"
+                     style={{backgroundImage: "url('/assets/images/production.jpg')"}}>
+                </div>
+                <div className="absolute h-[98%] w-[98%] bg-text opacity-20 rounded-[15px] z-[2]"></div>
+            </section>
+            <section id="houses-types"
+                     className="w-full max-w-[1200px] flex flex-col justify-center items-center text-text relative">
+                <ProductionBox/>
+            </section>
+        </>
+    )
+}
