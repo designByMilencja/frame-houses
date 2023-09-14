e0aea6c7aa104c4bd7cb166724ac710e6ff032fd
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-14 16:40:38 +0200

    feat(HeaderHome): added href props

--- components/home/HeaderHome.jsx
+++ components/home/HeaderHome.jsx
@@ -3,14 +3,14 @@ import {motion} from "framer-motion";
 import {move2} from "@/utils/motion";
 import Button from "@/components/Button";
 
-const HeaderHome = ({h1, h2, text}) => {
+const HeaderHome = ({h1, h2, text, href}) => {
     return (
         <motion.div
             variants={move2} initial="hidden" whileInView="show"
             className="flex flex-col justify-center items-center absolute h-[80%] z-[5] text-primary text-center">
             <h1 className="text-4xl xs:text-6xl uppercase text-with-shadow p-3"> {h1}</h1>
             <h2 className="text-2xl xs:text-3xl p-8">{h2}</h2>
-            <Button text={text}/>
+            <Button text={text} href={href}/>
         </motion.div>
     )
 }
