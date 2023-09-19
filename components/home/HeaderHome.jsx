88dd5f5e6eeece27414e3c39532b2870fdfc2ea1
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-19 14:20:35 +0200

    fix(): deleted class text-with-shadow

--- components/home/HeaderHome.jsx
+++ components/home/HeaderHome.jsx
@@ -8,7 +8,7 @@ const HeaderHome = ({h1, h2, text, href}) => {
         <motion.div
             variants={move2} initial="hidden" whileInView="show"
             className="flex flex-col justify-center items-center absolute h-[80%] z-[5] text-primary text-center">
-            <h1 className="text-4xl xs:text-6xl uppercase text-with-shadow p-3"> {h1}</h1>
+            <h1 className="text-4xl xs:text-6xl uppercase text-with-darkShadow p-3"> {h1}</h1>
             <h2 className="text-2xl xs:text-3xl p-8">{h2}</h2>
             <Button text={text} href={href}/>
         </motion.div>
