38d7b37f1217bb7c35b3858719230fad3ed52834
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-19 20:07:20 +0200

    feat(ProductSection): changed spaces

--- components/houses/ProductSection.jsx
+++ components/houses/ProductSection.jsx
@@ -1,12 +1,12 @@
 const ProductSection = ({ title, imageUrl, description }) => (
-    <div className="flex flex-col justify-evenly p-[30px]">
-        <h4 className="font-bold p-8 text-2xl">{title}</h4>
+    <div className="flex flex-col justify-evenly items-center px-[50px]">
+        <h4 className="font-bold p-6 text-2xl">{title}</h4>
         <div
-            className="w-[90%] h-[400px] bg-cover bg-center mx-auto rounded-[15px]"
+            className="w-[90%] h-[300px] bg-cover bg-center mx-auto rounded-[15px]"
             style={{ backgroundImage: `url(${imageUrl})` }}
         ></div>
-        <div className="flexCenter h-[200px] mt-[40px]">
-            <p className="w-[90%] mx-auto sm:w-[100%] p-[20px] leading-5 my-8 text-sm shadow rounded-[15px]">
+        <div className="flexCenter shadow my-[20px] rounded-[15px] m-[20px]">
+            <p className="w-[90%] mx-auto sm:w-[100%] p-6 leading-5 text-sm">
                 {description}
             </p>
         </div>
