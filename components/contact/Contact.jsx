a73d94ad70fdf10fa5665df80712429573f59864
Milena Pieńkosz <designbymilencja@gmail.com>
2023-09-20 14:37:47 +0200

    feat(Contact): changed contact structure

--- components/contact/Contact.jsx
+++ components/contact/Contact.jsx
@@ -1,53 +1,27 @@
 'use client'
-import Footer from "@/components/Footer";
-import Button from "@/components/Button";
-import {contact} from "@/constants";
+import {contact, contactInfo, contactLinks} from "@/constants";
 import Image from "next/image";
+import ContactInfo from "@/components/contact/ContactInfo";
+import ContactLinks from "@/components/contact/ContactLinks";
 
 const Contact = () => {
     return (
-        <section id="contact" className="w-full bg-green">
-            <div className="max-w-[1200px] mx-auto">
+        <section id="contact" className="max-w-[1200px] flexStart flex-col lg:px-20 py-6 px-5 w-full gap-20 bg-green">
+            <div className="flex flex-col gap-12 w-full">
                 <div className="absolute inset-0 clip-trapezoid-primary"></div>
-                <div className="flexCenter flex-col mb-[20px]">
-                    <div className="flexCenter mt-[80px]">
-                        {/*<img alt="logo icon - house" src="/assets/icons/home-outline.svg" className="w-[138px] m-5"/>*/}
-                        <Image src="/assets/icons/home-outline.svg" alt="home icon - logo" width={138} height={138} className="m-5"/>
-                        <div className="flex flex-col text-primary">
-                            <h2 className="font-extrabold text-2xl p-2">{contact.h2}</h2>
-                            <h3 className="text-1xl p-2">{contact.h3}</h3>
-                        </div>
-                    </div>
-                    <div className="flexCenter flex-col text-center">
-                        <div className="flex flex-col sm:flex-row">
-                            <div className="p-6 text-primary tracking-wider">
-                                <p className="text-2xl p-2">{contact.p1}</p>
-                                <p className="p-2">{contact.p2}</p>
-                                <p className="p-2">{contact.p3}</p>
-                                <a href="/privacy-policy" className="p-2">
-                                    {contact.a1}
-                                </a>
-                            </div>
-
-                            <div className="p-6 text-1xl text-primary">
-                                <p className="tracking-wider p-3">{contact.p4}</p>
-                                <Button text="kontakt@spacemobilehome.pl" href="mailto:kontakt@spacemobilehome.pl"/>
-                                <div className="flexCenter flex-wrap">
-                                    <Button text="Facebook" href="https://facebook.com"/>
-                                    <Button text="Instagram" href="https://instagram.com"/>
-                                    <Button text="Linkedin" href="https://linkedin.com"/>
-                                </div>
-                            </div>
-                            <div className="p-6 flexCenter sm:flex-col flex-wrap text-primary">
-                                <a href="/" className="p-2">{contact.a2}</a>
-                                <a href="/houses" className="p-2">{contact.a3}</a>
-                                <a href="/production" className="p-2">{contact.a4}</a>
-                            </div>
-                        </div>
+                <div className="flex items-center flex-col mt-5">
+                    <Image src="/assets/icons/home-outline.svg" alt="home icon - logo" width={138} height={138}/>
+                    <div className="flex flex-col text-primary text-center mt-2 max-w-xs">
+                        <h2 className="font-extrabold text-2xl p-2">{contact.h2}</h2>
+                        <h3 className="text-1xl p-2">{contact.h3}</h3>
                     </div>
                 </div>
+                <div className="flex flex-wrap gap-12 text-primary text-center">
+                    <ContactInfo title={contactInfo[0].title} infos={contactInfo[0].infos}/>
+                    <ContactLinks title={contactLinks[0].title} links={contactLinks[0].links}/>
+                    <ContactLinks title={contactLinks[1].title} links={contactLinks[1].links}/>
+                </div>
             </div>
-            <Footer/>
         </section>
     )
 }
