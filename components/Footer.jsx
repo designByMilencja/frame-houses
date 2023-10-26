8c408f39bf7b44a85f4cd0e8983a49547d44e67a
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-26 13:05:10 +0200

    fix(footer): changed tag for footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
         setYear(handleCurrentYear)
     }, [])
     return (
-        <div className="flex flex-row p-3 bg-primary w-full">
+        <footer className="flex flex-row p-3 bg-primary w-full">
             <p className="w-full text-green text-center text-xs"> <span>&copy;</span> Website: <a href="https://github.com/designByMilencja"  className="text-gradient ml-2"> DesignByMilencja </a>{year}</p>
-        </div>
+        </footer>
     )
 }
 export default Footer
