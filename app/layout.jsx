4c6b327b7a1fe56245f238ca60a5bbe593d979f4
Milena Pieńkosz <designbymilencja@gmail.com>
2023-10-26 19:21:58 +0200

    fix(): removed no needed brackets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import Navigation from "@/components/Navigation";
-import Contact from "@/components/contact/Contact";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer/Footer";
 
 export const metadata = {
     title: 'Space Mobile House - Strona główna',
@@ -12,15 +11,14 @@ export default function RootLayout({children}) {
     return (
         <html lang="pl">
         <body>
-        <div className={`w-full min-h-screen min-w-[300px] bg-primary overflow-hidden font-mont`}>
-            <header className={`h-[100%] mx-auto max-w-[1200px] flexCenter flex-col relative`}>
+        <div className="w-full min-h-screen min-w-[300px] bg-primary overflow-hidden font-mont">
+            <header className="h-[100%] mx-auto max-w-[1200px] flexCenter flex-col relative">
                 <Navigation/>
             </header>
             <main className="flexCenter flex-col relative">
                 {children}
             </main>
-            <div className={`h-[100%] w-full flexAround flex-col relative`}>
-                <Contact/>
+            <div className="h-[100%] w-full flexAround flex-col relative">
                 <Footer/>
             </div>
         </div>
